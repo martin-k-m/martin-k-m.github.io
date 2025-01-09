f978e333ccdf01c677839073135cb149e2c65aba
Martin Muskov <65186527+martin-k-m@users.noreply.github.com>
2025-01-08 18:47:07 -0800

    ds

--- src/App.tsx
+++ src/App.tsx
@@ -152,6 +152,8 @@ const App = () => {
                 </div>
             </footer>
         </div>
+
+
     );
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,3 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  base: 'https://github.com/martin-k-m/martin-k-m.github.io',
+};
